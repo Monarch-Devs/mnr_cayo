958ce5ceb80b6b6e6134de8c06fcaeb5cd797386
IlMelons <melonss2002@gmail.com>
2026-01-27 18:56:26 +0100

    Bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'mnr_cayo'
 description 'Cayo Perico Island Loader'
 author 'IlMelons'
-version '2.0.1'
+version '2.1.1'
 repository 'https://github.com/Monarch-Development/mnr_cayo'
 
 this_is_a_map 'yes'
@@ -13,5 +13,4 @@ this_is_a_map 'yes'
 client_scripts {
     'config/*.lua',
     'client/*.lua',
-
 }
